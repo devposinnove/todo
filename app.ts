743840fa4894ac39193bea3493f857e0517488ba
“mugishaj092” <mugishajoseph092@gmail.com>
2024-05-06 08:20:16 +0200

    Adding cors

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import todosRouter from './routes/todosRouter'
 import userRouter from './routes/useRouter'
+import cors from 'cors'
 
 // import swaggerjsdoc from 'swagger-jsdoc'
 import swaggerui from 'swagger-ui-express'
@@ -14,6 +15,7 @@ const app = express()
 //1)Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cors())
 app.use('/docs', swaggerui.serve, swaggerui.setup(documentation))
 app.use('/api/users', userRouter)
 app.use('/api/todos', todosRouter)
